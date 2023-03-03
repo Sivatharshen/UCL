932ac5a280736146611a279310bd9e5a400a2a7e
Sivatharshen <sivatharshen@gmail.com>
2023-03-03 18:35:39 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,29 @@
+def mat_to_list(adj_mat):
+    """ Convert adjacency matrix to an adjacency list representation
+
+    Parameters:
+    -----------
+    adj_mat : (a square 0-1 matrix)
+        Adjacency matrix (n x n) of the graph (of n nodes)
+
+
+    Returns:
+    --------
+     adj_list: `list[list[int]]`
+        The adjacency list of the graph
+
+    """
+    # TODO
+    adj_list = [[] for i in range(len(adj_mat[0]))]
+    for i in range(len(adj_mat[0])):
+        for j in range(len(adj_mat[0])):
+            if adj_mat[i][j] == 1:
+                adj_list[i].append(j)
+    return adj_list
+adj_mat =   [[0, 1, 0, 1, 0, 0],
+             [0, 0, 1, 0, 0, 0],
+             [1, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 1, 0],
+             [0, 0, 0, 1, 0, 0],
+             [0, 0, 0, 0, 0, 0]]
+print(mat_to_list(adj_mat))
